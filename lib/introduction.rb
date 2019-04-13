7d9f02e81d84b16927260b86f2ddf607681396c3
Nick Brozman <nick.brozman@gmail.com>
2019-04-13 18:28:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,11 @@
-def introduction(name, language)
-  puts "Hi, my name is #{name} and I'm learning to program #{language}."
+describe "#introduction" do 
+  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
+    expect{introduction("Nick")}.to output("Hi, my name is Nick.\n").to_stdout
+  end
+end
+
+describe "#introduction_with_language" do 
+  it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
+    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
+  end
 end
